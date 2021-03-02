5d05d6997dd3ac82b7decde0d84efa1d3ba05ff3
JanMeckelholt <Jan_Meckelholt@web.de>
2021-03-02 20:34:32 +0100

    bootstrap cards for stich

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const http = require('http')
-const app = require('express')()
-app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"))
+
+//const app = require('express')()
+//app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"))
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap-show-modal/src')); 
 app.listen(9091, ()=> console.log("Listening on http port 9091"))
 const httpServer = http.createServer()
 const websocketServer = require('websocket').server
@@ -38,27 +46,81 @@ wsServer.on("request", request => {
             connection.send(JSON.stringify(payLoad))
         }       
         if (result.method === "joinGame"){
-            console.log("Trying to join a Game! ClientId: " + result.clientId + " GameId: " + result.gameId )
-            const clientId = result.clientId
-            const gameId = result.gameId;
+            console.log("Trying to join a Game! ClientId: " + result.player.clientId + " GameId: " + result.gameId )
+            const player = result.player
+            const gameId = result.gameId
+           // const clientId = player.clientId
+           // const nickName = player.nickName
             let players = games[gameId].players
-            players.push(clientId)
+            let payLoad = {}
+            if (players.length < 4){
+                players.push(player)
+                games[gameId] = {
+                    "id": gameId,
+                    "players": players
+                }
+                if (players.length < 4){
+                    payLoad = {
+                        "method": "gameJoined",
+                        "game": games[gameId],
+                        "playerJoined": player
+                    }
+                } else {
+                    payLoad = {
+                        "method": "gameStarted",
+                        "game": games[gameId],
+                        "playerJoined": player
+                    }
+                }
+                console.log("game.id: " + games[gameId].id)
+                players = games[gameId].players
+                games[gameId].players.forEach(player => {
+                    const connection = clients[player.clientId].connection
+                    connection.send(JSON.stringify(payLoad))
+                })
+                    
+            } else {
+                const payLoad = {
+                    "method": "gameFull",
+                    "game": games[gameId]
+                }
+                const connection = clients[player.clientId].connection
+                connection.send(JSON.stringify(payLoad))
+            }
+        }
+        if (result.method === "leaveGame"){
+            console.log("Trying to leave Game " + result.gameId + ". ClientId: " + result.player.clientId )
+            const gameId = result.gameId
+            const player = result.player
+            const clientId = player.clientId
+            
+            players = games[gameId].players
+            players = players.filter((p, index, arr) =>{
+                return p.clientId != player.clientId
+            })
+            // const index = players.indexOf(player);
+            // if (index > -1) {
+            // players.splice(index, 1);
+            // }
             games[gameId] = {
                 "id": gameId,
                 "players": players
             }
-            const payLoad = {
-                "method": "gameJoined",
-                "game": games[gameId]
+            var payLoad = {
+                "method": "playerLeft",
+                "game": games[gameId],
+                "playerLeft": player
             }
-            console.log("game.id: " + games[gameId].id)
-            players = games[gameId].players
-            console.log("Players: " + players)
             games[gameId].players.forEach(player => {
-                const connection = clients[player].connection
+                const connection = clients[player.clientId].connection
                 connection.send(JSON.stringify(payLoad))
-             })
-        }
+            })
+            payLoad = {
+                "method": "left",
+                "player": player,
+            }
+            connection.send(JSON.stringify(payLoad))
+        }  
     })
 
     const clientId = uuidv4()
---
+++ public/main.js
@@ -0,0 +1,161 @@
+
+
+const startPage = document.getElementById("startPage")
+const waitPage = document.getElementById("waitPage")
+const playPage = document.getElementById("playPage")
+//const btnNewGame = document.getElementById("btnNewGame")
+const chkNewGame = document.getElementById("chkNewGame")
+const btnJoinGame = document.getElementById("btnJoinGame")
+const btnLeaveGame = document.getElementById("btnLeaveGame")
+const txtGameId = document.getElementById("txtGameId")
+const txtNickName = document.getElementById("txtNickName")
+const lblJoinedPlayers = document.getElementById("lblJoinedPlayers")
+txtGameId.value = ''
+txtNickName.value = ''
+chkNewGame.checked = false
+// lblJoinedPlayers.innerText = ''
+
+// btnNewGame.addEventListener("click", e => {
+//     const payLoad = {
+//         "method": "newGame",
+//         "clientId": clientId
+//     }
+//     ws.send(JSON.stringify(payLoad))
+// })
+chkNewGame.addEventListener("change", e => {
+    console.log("Checkbox changed!")
+    if (e.target.checked){
+        if (gameId === null){
+            const payLoad = {
+                "method": "newGame",
+                "clientId": player.clientId
+            }
+            ws.send(JSON.stringify(payLoad))
+        }
+        txtGameId.value=gameId
+        txtGameId.readOnly = true
+    }
+    else {
+        txtGameId.readOnly = false
+    }
+})
+
+btnJoinGame.addEventListener("click", e => {
+    if (txtNickName.value ==''){
+        $.showAlert({title: "Join a Game", body: "Nickname can not be empty."})
+        txtNickName.focus()
+        txtNickName.select()
+    } else if(!isValid(txtGameId.value)){
+            $.showAlert({title: "Join a Game", body: "Game ID is not valid."})
+            txtGameId.focus()
+            txtGameId.select()
+            } else {
+                if (gameId === null){
+                    gameId = txtGameId.value
+                }
+                player.nickName=txtNickName.value
+                const payLoad = {
+                    "method": "joinGame",
+                    "player": player,
+                    "gameId": gameId
+                }
+                ws.send(JSON.stringify(payLoad))
+            }
+})      
+
+btnLeaveGame.addEventListener("click", e => {
+    const payLoad = {
+        "method": "leaveGame",
+        "player": player,
+        "gameId": gameId
+    }
+    ws.send(JSON.stringify(payLoad))
+})
+
+
+let player = {clientId: null, nickName: null}
+//let clientId = null
+//let nickName = null
+let gameId = null
+let joinedPlayers = null
+let ws = new WebSocket("ws://localhost:9090")
+ws.onmessage = message => {
+    const response = JSON.parse(message.data)
+    console.log("Response Method: " + response.method)
+    if (response.method === "connect"){
+        player.clientId = response.clientId
+        startPage.style.display = 'block'
+        waitPage.style.display = 'none'
+        console.log("ClientId set to: "  + player.clientId)
+    } 
+    if (response.method === "newGameCreated"){
+        gameId = response.game.id
+        console.log("Game with id " + gameId + " created!")
+        txtGameId.value = gameId
+    }  
+    if (response.method === "gameJoined"){
+        gameId = response.game.id
+        console.log("Game with id " + gameId + " joined!")
+        //startPage.remove()
+        startPage.style.display = 'none'
+        waitPage.style.display = 'block'
+        // waitPage.style.visibility = true
+        txtGameId.value = gameId
+        lblJoinedPlayers.innerText='You joined Game: ' + gameId
+        lblJoinedPlayers.innerHTML += '<br/> The following player are waiting with you: <br/>'
+        response.game.players.forEach(player =>{
+            lblJoinedPlayers.innerText += player.nickName 
+            lblJoinedPlayers.innerHTML += '<br/>' 
+        })
+    }    
+    if (response.method === "gameStarted"){
+        gameId = response.game.id
+        console.log("Game with id " + gameId + " started!")
+        //startPage.remove()
+        startPage.style.display = 'none'
+        waitPage.style.display = 'none'
+        playPage.style.display = 'block'
+        // waitPage.style.visibility = true
+        // txtGameId.value = gameId
+        // lblJoinedPlayers.innerText='You joined Game: ' + gameId
+        // lblJoinedPlayers.innerHTML += '<br/> The following player are waiting with you: <br/>'
+        // response.game.players.forEach(player =>{
+        //     lblJoinedPlayers.innerText += player.nickName 
+        //     lblJoinedPlayers.innerHTML += '<br/>' 
+        // })
+    }
+    if (response.method === "gameFull"){
+        gameId = response.game.id
+        console.log("Game with id " + gameId + " is full")
+        txtGameId.value = gameId
+        $.showAlert({title: "Join Game", body: "Game with id " + gameId + " is full"})
+    }  
+    if (response.method === "playerLeft"){
+        gameId = response.game.id
+        const playerLeft = response.playerLeft
+        console.log(playerLeft.nickName + " left the game")
+        txtGameId.value = gameId
+        $.showAlert({title: "Player left", body: playerLeft.nickName + " left the game."})
+        //lblJoinedPlayers.innerText= playerLeft. + " left the game"
+        lblJoinedPlayers.innerText= ''
+        lblJoinedPlayers.innerHTML += '<br/> The following player are waiting with you: <br/>'
+        response.game.players.forEach(player =>{
+            lblJoinedPlayers.innerText += player.nickName 
+            lblJoinedPlayers.innerHTML += '<br/>' 
+        })
+    } 
+    if (response.method === "left"){
+        player.clientId = response.player.clientId
+        player.checkednickName = response.player.nickName
+        startPage.style.display = 'block'
+        waitPage.style.display = 'none'
+        txtNickName.value = player.nickName
+        chkNewGame.checked=false
+    }  
+}
+
+function isValid(testText) {
+    var pattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return pattern.test(testText)
+}
+  
